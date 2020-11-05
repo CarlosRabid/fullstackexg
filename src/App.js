2826e6650c3de2d7338c5a0f35f9d2059796b3dd
Carlos Cornejo <carlitos.cornejo.n@hotmail.com>
2020-11-05 18:57:27 -0500

    finished version 1.0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { TextField, Snackbar, Button, Tooltip } from "@material-ui/core";
 import axios from "axios";
+import Navmenu from "./Components/actions/navmenu";
+import Cardsingle from "./Components/actions/cardsingle";
+import Openings from "./Components/actions/openings";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      jobs: [],
       name: "",
       snack: false,
+      card: false,
+      expanded: false,
+      openings: false,
     };
   }
   update = async (event) => {
@@ -23,19 +30,40 @@ class App extends Component {
       });
     }
   };
-  async checker() {
-    let data = await axios.get("http://localhost:4328/bios");
-    let username = { ...this.state.name };
+  checker = async () => {
+    let data = await axios.get(`http://localhost:4328/bios/${this.state.name}`);
     let nodata = false;
     if (data.data[0]) {
       nodata = true;
     } else {
       nodata = false;
+      return this.setState({
+        data: data.data,
+        card: true,
+      });
     }
-    return this.setState({
-      data: data.data,
+    return;
+  };
+
+  jobChecker = async () => {
+    let offset = 0;
+    let size = 1;
+    let aggregate = true;
+    let response = await axios.post(`http://localhost:4328/jobs`, {
+      data: { offset, size, aggregate },
     });
-  }
+    let nodata = false;
+    if (response.data.results[0]) {
+      nodata = true;
+    } else {
+      nodata = false;
+      return this.setState({
+        jobs: response.data.results,
+        openings: true,
+      });
+    }
+    return;
+  };
 
   closeModal = async () => {
     let timeout = 5000;
@@ -62,6 +90,14 @@ class App extends Component {
     }
   };
 
+  handleMenu = async (input) => {
+    await this.setState({ input });
+  };
+
+  handleExpandClick = async (prevExpanded) => {
+    await this.setState({ expanded: !prevExpanded });
+  };
+
   render() {
     return (
       <div className="App">
@@ -79,27 +115,76 @@ class App extends Component {
         </Button>
         <br />
         <br />
-        <TextField
-          required
-          id="name"
-          label="Required"
-          value={this.state.name}
-          onChange={this.update}
-        />
-        <Tooltip title="search username">
-          <Button
-            variant="contained"
-            onClick={this.checker}
-            style={{ marginLeft: "4%" }}
-            size="medium"
-            position="end"
-            color="primary"
-            name="editmode"
-            id="editmode"
-          >
-            Check Username
-          </Button>
-        </Tooltip>
+        {this.state.input === "Singular" ? (
+          <div>
+            {" "}
+            <TextField
+              required
+              id="name"
+              label="Required"
+              value={this.state.name}
+              onChange={this.update}
+            />
+            <Button
+              variant="contained"
+              onClick={this.checker}
+              style={{ marginLeft: "4%" }}
+              size="medium"
+              position="end"
+              color="primary"
+              name="editmode"
+              id="editmode"
+            >
+              Check Username
+            </Button>
+          </div>
+        ) : (
+          <></>
+        )}
+        {this.state.card && this.state.input === "Singular" ? (
+          <div>
+            <Cardsingle
+              expanded={this.state.expanded}
+              data={this.state.data}
+              handleExpandClick={this.handleExpandClick}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+        {this.state.input === "Job Openings" ? (
+          <>
+            {/* <TextField
+                  required
+                  id="Latest Software Development Offerings"
+                  label="Required"
+                  value={this.state.name}
+                  onChange={this.update}
+                /> */}
+            <Button
+              variant="contained"
+              onClick={this.jobChecker}
+              style={{ marginLeft: "4%" }}
+              size="medium"
+              position="end"
+              color="primary"
+              name="resultspp"
+              id="resultspp"
+            >
+              Check Results
+            </Button>
+          </>
+        ) : (
+          <></>
+        )}
+        {this.state.openings ? (
+          <Openings
+            jobs={this.state.jobs}
+            handleExpandClick={this.handleExpandClick}
+          />
+        ) : (
+          <></>
+        )}
         {this.state.snack ? (
           <Snackbar
             anchorOrigin={{
@@ -113,6 +198,7 @@ class App extends Component {
         ) : (
           <></>
         )}
+        <Navmenu handleMenu={this.handleMenu} />
       </div>
     );
   }
