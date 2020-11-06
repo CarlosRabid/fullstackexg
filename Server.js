cedf718c77018c129d71b2e58ba39484c7754896
Carlos Cornejo <carlitos.cornejo.n@hotmail.com>
2020-11-05 23:42:01 -0500

    deploying

--- Server.js
+++ Server.js
@@ -107,7 +107,7 @@ app.post("/jobs", async function (req, res) {
   });
 });
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
